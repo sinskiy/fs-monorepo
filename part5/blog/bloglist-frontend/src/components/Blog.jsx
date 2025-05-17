a998f9b120257ab36c6d3cdd636e84b6fe0fcdcc
sinskiy <dimasinskiy@gmail.com>
2025-05-17 11:49:40 +0300

    feat: add blog details toggle button

--- part5/blog/bloglist-frontend/src/components/Blog.jsx
+++ part5/blog/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const [isVisible, setIsVisible] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setIsVisible(!isVisible)}>
+          {isVisible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {isVisible && (
+        <>
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes}
+            <button>like</button>
+          </p>
+          <p>{blog.user?.username}</p>
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
